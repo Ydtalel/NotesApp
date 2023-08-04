57a8b16850db83e8e0ba952c4e5c54783523abbc
Мироненко Юрий <ydtalel@gmail.com>
2023-08-05 00:30:41 +0300

    crud operations added

--- js/script.js
+++ js/script.js
@@ -2,79 +2,114 @@
 const noteForm = document.getElementById('noteForm');
 const noteTitle = document.getElementById('noteTitle');
 const noteContent = document.getElementById('noteContent');
+const importantNote = document.getElementById('importantNote');
 const notesList = document.getElementById('notesList');
 const importantFilter = document.getElementById('importantFilter');
+const searchInput = document.getElementById('searchInput');
 
 // Получаем заметки из localStorage или создаем пустой массив
 let notes = JSON.parse(localStorage.getItem('notes')) || [];
 
 // Функция отображения заметок в списке
-function displayNotes() {
-    notesList.innerHTML = '';
+function displayNotes(notesToDisplay = notes) {
+  notesList.innerHTML = '';
 
-    const filteredNotes = importantFilter.checked
-        ? notes.filter((note) => note.important)
-        : notes;
+  notesToDisplay.forEach((note, index) => {
+    const noteDiv = document.createElement('div');
+    noteDiv.classList.add('note');
+    noteDiv.classList.add('card', 'mb-3');
+    noteDiv.innerHTML = `
+        <div class="card-body ${note.important ? 'important-note' : ''}">
+          <h3 class="card-title">${note.title}</h3>
+          <p class="card-text">${note.content}</p>
+          <button class="btn btn-dark" onclick="editNote(${index})">Редактировать</button>
+          <button class="btn btn-danger" onclick="deleteNote(${index})">Удалить</button>
+        </div>
+    `;
 
-    filteredNotes.forEach((note, index) => {
-        const noteDiv = document.createElement('div');
-        noteDiv.classList.add('note');
-        noteDiv.innerHTML = `
-            <h3>${note.title}</h3>
-            <p>${note.content}</p>
-            <div class="note-actions">
-                <button onclick="editNote(${index})">Редактировать</button>
-                <button onclick="deleteNote(${index})">Удалить</button>
-            </div>
-        `;
-        notesList.appendChild(noteDiv);
-    });
+    notesList.appendChild(noteDiv);
+  });
 }
+
 
 // Функция добавления новой заметки
 function addNote() {
-    const title = noteTitle.value;
-    const content = noteContent.value;
-    const newNote = { title, content, important: false };
-    notes.push(newNote);
+  const title = noteTitle.value;
+  const content = noteContent.value;
+  const important = importantNote.checked; // Получаем значение из чекбокса
+  const newNote = { title, content, important };
+  notes.push(newNote);
 
-    // Сохраняем заметки в localStorage
-    localStorage.setItem('notes', JSON.stringify(notes));
+  // Сохраняем заметки в localStorage
+  localStorage.setItem('notes', JSON.stringify(notes));
 
-    // Очищаем поля формы и обновляем список заметок
-    noteTitle.value = '';
-    noteContent.value = '';
-    displayNotes();
+  // Очищаем поля формы и обновляем список заметок
+  noteTitle.value = '';
+  noteContent.value = '';
+  importantNote.checked = false; // Сбрасываем чекбокс в неотмеченное состояние
+  displayNotes();
 }
 
 // Функция редактирования заметки
 function editNote(index) {
-    const editedTitle = prompt('Введите новый заголовок', notes[index].title);
-    const editedContent = prompt('Введите новый текст заметки', notes[index].content);
+  const editedTitle = prompt('Введите новый заголовок', notes[index].title);
+  const editedContent = prompt('Введите новый текст заметки', notes[index].content);
 
-    if (editedTitle !== null && editedContent !== null) {
-        notes[index].title = editedTitle;
-        notes[index].content = editedContent;
-        localStorage.setItem('notes', JSON.stringify(notes));
-        displayNotes();
-    }
+  if (editedTitle !== null && editedContent !== null) {
+    const important = importantNote.checked; // Получаем значение из чекбокса
+    notes[index].title = editedTitle;
+    notes[index].content = editedContent;
+    notes[index].important = important; // Обновляем значение важности
+    localStorage.setItem('notes', JSON.stringify(notes));
+    displayNotes();
+  }
 }
 
 // Функция удаления заметки
 function deleteNote(index) {
-    notes.splice(index, 1);
-    localStorage.setItem('notes', JSON.stringify(notes));
-    displayNotes();
+  notes.splice(index, 1);
+  localStorage.setItem('notes', JSON.stringify(notes));
+  displayNotes();
+}
+
+// Функция поиска заметок по фрагменту текста в заголовке или тексте
+function searchNotes(searchTerm) {
+  const filteredNotes = notes.filter(
+    (note) =>
+      note.title.toLowerCase().includes(searchTerm.toLowerCase()) 
+      || note.content.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  displayNotes(filteredNotes);
 }
 
 // Обработчик отправки формы для добавления заметки
 noteForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    addNote();
+  event.preventDefault();
+  addNote();
 });
 
 // Обработчик изменения фильтра
-importantFilter.addEventListener('change', displayNotes);
+importantFilter.addEventListener('change', () => {
+  const filteredNotes = importantFilter.checked
+    ? notes.filter((note) => note.important)
+    : notes;
+  displayNotes(filteredNotes);
+});
 
-// При загрузке страницы отображаем список заметок
+// Обработчик изменения поля поиска
+searchInput.addEventListener('input', () => {
+  const searchTerm = searchInput.value.trim();
+  searchNotes(searchTerm);
+});
+
+function applyImportantStyles(noteDiv, isImportant) {
+  if (isImportant) {
+    noteDiv.style.backgroundColor = '#f8d7da'; // Измените на тот цвет, который вы хотите
+  } else {
+    noteDiv.style.backgroundColor = ''; // Сбрасываем фон, если заметка не важная
+  }
+}
+
+
+// При загрузке страницы отображаем список всех заметок
 displayNotes();
